81728059c1e0e2b18cec81c174a00289997e92d4
AncyStar <ancystarz@gmail.com>
2025-02-25 23:32:34 +0530

    commit

--- src/api/passwordReset.jsx
+++ src/api/passwordReset.jsx
@@ -18,7 +18,7 @@ export const requestPasswordReset = async (email) => {
 export const resetPassword = async (token, password) => {
   try {
     const response = await axios.post(
-      `https://password-reset-backend.onrender.com/reset-password/${token}`,
+      `https://password-reset-3inm.onrender.com/reset-password/${token}`,
       { password }
     );
     return response.data;
