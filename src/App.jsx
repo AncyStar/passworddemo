4da3c42a5d195c05a5cdbfd2fa1a5e4fed1cb2b6
AncyStar <ancystarz@gmail.com>
2025-02-28 13:25:15 +0530

    ewe

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,13 @@ function App() {
       <div className="container mt-5">
         <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4 shadow-sm rounded">
           <div className="container-fluid">
-            <Link className="navbar-brand" href="/">
+            <Link className="navbar-brand" to="/">
               Password Reset App
             </Link>
             <div className="collapse navbar-collapse">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <Link className="nav-link" href="/forgot-password">
+                  <Link className="nav-link" to="/forgot-password">
                     Forgot Password
                   </Link>
                 </li>
@@ -37,6 +37,11 @@ function App() {
                 <li className="nav-item">
                   <Link className="nav-link" to="/login">
                     Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/reset-password">
+                    Reset Password
                   </Link>
                 </li>
               </ul>
